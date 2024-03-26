8c79d17ad01face1a2a5989bec966f0e216859be
Pierre Thiébaud <119924362+WTHIJ@users.noreply.github.com>
2024-03-26 16:14:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const ytdl = require('ytdl-core');
+const ProgressBar = require('progress');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,12 +10,23 @@ const rl = readline.createInterface({
 
 rl.question('Entrez le lien de la vidéo YouTube : ', (videoUrl) => {
   rl.question('Entrez le nom du fichier de sortie (sans extension) : ', (fileName) => {
-    const outputFilePath = `${fileName}.mp4`; 
+    const outputFilePath = `${fileName}.mp4`;
+    const videoStream = ytdl(videoUrl, { quality: 'highestvideo', filter: 'videoandaudio' });
+    const fileSize = fs.statSync(outputFilePath).size;
+    const progressBar = new ProgressBar('[:bar] :percent :etas', {
+      complete: '=',
+      incomplete: ' ',
+      width: 20,
+      total: fileSize
+    });
 
-    ytdl(videoUrl, { quality: 'highestvideo', filter: 'videoandaudio' })
-      .pipe(fs.createWriteStream(outputFilePath))
+    videoStream.on('progress', (chunkLength, downloaded, total) => {
+      progressBar.tick(chunkLength);
+    });
+
+    videoStream.pipe(fs.createWriteStream(outputFilePath))
       .on('finish', () => {
-        console.log('Téléchargement terminé !');
+        console.log('\nTéléchargement terminé !');
       })
       .on('error', (error) => {
         console.error('Une erreur est survenue lors du téléchargement :', error);
